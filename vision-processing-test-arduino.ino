2653074ce32511f123afb545b806e130ab0915ac
Caleb Denio <cjdenio44@gmail.com>
2019-01-28 20:23:19 -0500

    Limited transmissions to once every 2000 frames

--- vision-processing-test-arduino.ino
+++ vision-processing-test-arduino.ino
@@ -8,6 +8,7 @@ int x;
 int y;
 int width = 0;
 int height = 0;
+int i = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,20 +16,24 @@ void setup() {
 
   pixy.init();
 }
-int j;
 
 void loop() {
  numBlocks = pixy.getBlocks();
+ i++;
  
  if(numBlocks){
     x = pixy.blocks[0].x;
     y = pixy.blocks[0].y;
     width = pixy.blocks[0].width;
     height = pixy.blocks[0].height;
-    
+  }
+
+  if (i == 2000) {
     //Don't char your cookies!
     char cookies[32];
-    sprintf(cookies, "%d,%d,%d,%d", x, y, width, height); 
+    sprintf(cookies, "%d,%d,%d,%d", x, y, width, height);
     Serial.println(cookies);
+
+    i = 0;
   }
- }
+}

9c016e0f72bfb678fcf671847d0e9e6eb58e4aeb
Caleb Denio <cjdenio44@gmail.com>
2019-02-10 14:20:33 -0500

    Added cool I2C stuff.

--- vision-processing-test-arduino.ino
+++ vision-processing-test-arduino.ino
@@ -1,39 +1,35 @@
-#include <Pixy.h>
+#include <Wire.h>
 #include <SPI.h>
+#include <Pixy.h>
 
 Pixy pixy;
 
-int numBlocks;
-int x;
-int y;
-int width = 0;
-int height = 0;
-int i = 0;
+int x, y, width, height;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(19200);
+  Wire.begin(4);
+  Wire.onRequest(requestEvent);
 
   pixy.init();
 }
-
+String dataToSend;
+int blocks;
 void loop() {
- numBlocks = pixy.getBlocks();
- i++;
- 
- if(numBlocks){
-    x = pixy.blocks[0].x;
-    y = pixy.blocks[0].y;
-    width = pixy.blocks[0].width;
-    height = pixy.blocks[0].height;
+  // put your main code here, to run repeatedly:
+  blocks = pixy.getBlocks();
+  if(blocks){
+      x = pixy.blocks[0].x;
+      y = pixy.blocks[0].y;
+      width = pixy.blocks[0].width;
+      height = pixy.blocks[0].height;
   }
+  
+  delay(20);
+}
 
-  if (i == 2000) {
-    //Don't char your cookies!
-    char cookies[32];
-    sprintf(cookies, "%d,%d,%d,%d", x, y, width, height);
-    Serial.println(cookies);
-
-    i = 0;
-  }
+void requestEvent(){
+  char cookies[32];
+  sprintf(cookies, "%d,%d,%d,%d", x, y, width, height);
+  Wire.write(cookies);
 }
